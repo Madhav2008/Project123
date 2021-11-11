c94caa749dd22ed58b05c15f9877e5281869cd67
Madhav Arora <72864817+Madhav2008@users.noreply.github.com>
2021-11-11 12:38:16 +0530

    Update Project123.py

--- Project123.py
+++ Project123.py
@@ -11,8 +11,8 @@ from PIL import Image
 import PIL.ImageOps
 import os, ssl, time
 
-X = np.load('./image.npz')['arr_0']
-y = pd.read_csv("./labels.csv")["labels"]
+X = np.load('image.npz')['arr_0']
+y = pd.read_csv("labels.csv")["labels"]
 print(pd.Series(y).value_counts())
 classes = ['A', 'B', 'C', 'D', 'E','F', 'G', 'H', 'I', 'J', "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 nclasses = len(classes)
